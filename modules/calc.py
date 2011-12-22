28878e82e1e32c1efa7829e4029f9f48a4d8e643
Dimitri Molenaars <Tyrope@TyRope.nl>
2011-12-22 17:06:57 +0100

    deprecating .calc properly
    Signed-off-by: Dimitri Molenaars <Tyrope@TyRope.nl>

--- modules/calc.py
+++ modules/calc.py
@@ -10,62 +10,6 @@ http://inamidst.com/phenny/
 
 import re, string, web
 
-r_result = re.compile(r'(?i)<A NAME=results>(.*?)</A>')
-r_tag = re.compile(r'<\S+.*?>')
-
-subs = [
-	(' in ', ' -> '),
-	(' over ', ' / '),
-	(u'£', 'GBP '),
-	(u'€', 'EUR '),
-	('\$', 'USD '),
-	(r'\bKB\b', 'kilobytes'),
-	(r'\bMB\b', 'megabytes'),
-	(r'\bGB\b', 'kilobytes'),
-	('kbps', '(kilobits / second)'),
-	('mbps', '(megabits / second)')
-]
-
-def calc(phenny, input):
-	"""Use the Frink online calculator."""
-	q = input.group(2)
-	if not q:
-		return phenny.say('0?')
-
-	query = q[:]
-	for a, b in subs:
-		query = re.sub(a, b, query)
-	query = query.rstrip(' \t')
-
-	precision = 5
-	if query[-3:] in ('GBP', 'USD', 'EUR', 'NOK'):
-		precision = 2
-	query = web.urllib.quote(query.encode('utf-8'))
-
-	uri = 'http://futureboy.us/fsp/frink.fsp?fromVal='
-	bytes = web.get(uri + query)
-	m = r_result.search(bytes)
-	if m:
-		result = m.group(1)
-		result = r_tag.sub('', result) # strip span.warning tags
-		result = result.replace('&gt;', '>')
-		result = result.replace('(undefined symbol)', '(?) ')
-
-		if '.' in result:
-			try: result = str(round(float(result), precision))
-			except ValueError: pass
-
-		if not result.strip():
-			result = '?'
-		elif ' in ' in q:
-			result += ' ' + q.split(' in ', 1)[1]
-
-		phenny.say(q + ' = ' + result[:350])
-	else: phenny.reply("Sorry, can't calculate that.")
-	phenny.say('Note that .calc is deprecated, consider using .c')
-calc.commands = ['calc']
-calc.example = '.calc 5 + 3'
-
 def c(phenny, input):
 	"""Google calculator."""
 	if not input.group(2):
@@ -87,7 +31,7 @@ def c(phenny, input):
 		answer = web.decode(answer)
 		phenny.say(answer)
 	else: phenny.say('Sorry, no result.')
-c.commands = ['c']
+c.commands = ['c','calc']
 c.example = '.c 5 + 3'
 
 def py(phenny, input):
